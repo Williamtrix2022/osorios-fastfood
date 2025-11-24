9f3e9e3f4e6505da6ec04e30edca3611afe20d6e
Jhon Quiceno <quicenojale@gmail.com>
2025-11-24 13:50:12 -0500

    refactor: update product prices from USD to Colombian pesos (COP)

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -15,7 +15,7 @@ class ProductoSeeder extends Seeder
                 'categoria_id' => 1,
                 'nombre' => 'Hamburguesa Clásica',
                 'descripcion' => 'Carne de res, queso, lechuga, tomate y salsa especial',
-                'precio' => 8.99,
+                'precio' => 28000,
                 'imagen' => null,
                 'disponible' => true,
             ],
@@ -23,7 +23,7 @@ class ProductoSeeder extends Seeder
                 'categoria_id' => 1,
                 'nombre' => 'Hamburguesa Doble',
                 'descripcion' => 'Doble carne, doble queso, lechuga y barbacoa',
-                'precio' => 12.99,
+                'precio' => 38000,
                 'imagen' => null,
                 'disponible' => true,
             ],
@@ -31,17 +31,17 @@ class ProductoSeeder extends Seeder
                 'categoria_id' => 1,
                 'nombre' => 'Hamburguesa Especial',
                 'descripcion' => 'Carne premium, queso cheddar, cebolla caramelizada',
-                'precio' => 15.99,
+                'precio' => 45000,
                 'imagen' => null,
                 'disponible' => true,
             ],
-            
+
             // Salchipapas (categoria_id: 2)
             [
                 'categoria_id' => 2,
                 'nombre' => 'Salchipapa Clásica',
                 'descripcion' => 'Papas fritas, salchicha, queso y salsas',
-                'precio' => 6.99,
+                'precio' => 18000,
                 'imagen' => null,
                 'disponible' => true,
             ],
@@ -49,37 +49,37 @@ class ProductoSeeder extends Seeder
                 'categoria_id' => 2,
                 'nombre' => 'Salchipapa Especial',
                 'descripcion' => 'Papas, salchicha, pollo desmenuzado, queso y salsas',
-                'precio' => 9.99,
+                'precio' => 26000,
                 'imagen' => null,
                 'disponible' => true,
             ],
-            
+
             // Picadas (categoria_id: 3)
             [
                 'categoria_id' => 3,
                 'nombre' => 'Picada Mixta',
                 'descripcion' => 'Carne de res, pollo, chorizo, papas y plátano',
-                'precio' => 15.99,
+                'precio' => 42000,
                 'imagen' => null,
                 'disponible' => true,
             ],
-            
+
             // Perros (categoria_id: 4)
             [
                 'categoria_id' => 4,
                 'nombre' => 'Perro Sencillo',
                 'descripcion' => 'Salchicha, pan, papitas y salsas',
-                'precio' => 4.99,
+                'precio' => 12000,
                 'imagen' => null,
                 'disponible' => true,
             ],
-            
+
             // Bebidas (categoria_id: 5)
             [
                 'categoria_id' => 5,
                 'nombre' => 'Gaseosa',
                 'descripcion' => 'Coca Cola, Sprite o Fanta',
-                'precio' => 2.50,
+                'precio' => 5000,
                 'imagen' => null,
                 'disponible' => true,
             ],
@@ -87,7 +87,7 @@ class ProductoSeeder extends Seeder
                 'categoria_id' => 5,
                 'nombre' => 'Jugo Natural',
                 'descripcion' => 'Jugos de frutas naturales',
-                'precio' => 3.50,
+                'precio' => 8000,
                 'imagen' => null,
                 'disponible' => true,
             ],
